f264363bf141c8e15388c72fb1274cc8912f4f5d
Lazaro Mbaila <lazarozito@gmail.com>
2025-11-10 20:15:31 +0200

    fix connection

--- src/infrastructure/prisma/prisma.service.ts
+++ src/infrastructure/prisma/prisma.service.ts
@@ -1,15 +1,20 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
-    (this.$on as any)('beforeExit', async () => {
+    // Forca o tipo para contornar o erro TS2345
+    (this as any).$on('beforeExit', async () => {
       await app.close();
     });
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
